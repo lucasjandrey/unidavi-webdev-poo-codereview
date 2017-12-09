e27e7a24311398d6c8927154b2735f64db8d1cce
Lucas Jandrey <lucasjandrey@gmail.com>
2017-12-09 11:35:40 -0200

    Criar site

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
+import java.util.logging.Logger;
 
+public class App {
+  public static void main(String[] args){
+	  Logger.getGlobal().info("Hello");
+  }
 }
